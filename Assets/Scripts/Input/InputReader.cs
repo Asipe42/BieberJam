14cf745a4ca4a605e5a350cff3ba1325448eedd7
Eom Jae Cheon <86538738+Asipe42@users.noreply.github.com>
2022-11-26 04:22:31 +0900

    공격 카메라 효과

--- Assets/Scripts/Input/InputReader.cs
+++ Assets/Scripts/Input/InputReader.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class InputReader : MonoBehaviour
 {
     // Inspector
     [SerializeField] private TreeSpawner _treeSpawner;
+
+    Animator anim;
+
+    Vector3 originCameraPosition;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+
+        originCameraPosition = Camera.main.transform.position;
+    }
 
     void Update()
     {
@@ -24,6 +36,13 @@ public class InputReader : MonoBehaviour
             _treeSpawner.SpawnTree();
             AudioManager.instance.PlaySFX(SFXDefiniton.SFX_ATTACK);
             HP.instance.RecoverHP();
+            anim.SetTrigger("Attack");
+            // Camera.main.DOShakePosition(0.2f);
+
+            var sequence = DOTween.Sequence();
+            sequence.Append(Camera.main.DOOrthoSize(4.8f, 0.1f))
+                    .Append(Camera.main.DOOrthoSize(5f, 0.2f));
+                    
         }
     }
 }
